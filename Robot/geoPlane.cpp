f59617276e7b133ae6dadd15b2bb90ea07a46a56
Andy <chalendard.andy@gmail.com>
2019-12-03 00:30:17 +0100

    Robot: Calcul fermeture geo dans le plan

--- Robot/geoPlane.cpp
+++ Robot/geoPlane.cpp
@@ -6,6 +6,8 @@
 #define VAL_C 285   // Longueur avant-bras
 #define VAL_D 34    // Hauteur main
 
+#define DELTA_L 1    // Précision pour la valeur de l
+
 void GeoPlane::setAngle(float teta1, float teta2) {
     tetaIsCalc = true;
     coordIsCalc = false;
@@ -23,10 +25,43 @@ void GeoPlane::setCoord(float l, float h) {
 }
 
 void GeoPlane::getAngle(float & teta1, float & teta2) {
-    // TODO
+    float tmp;
+    float teta1Min, teta1Max;
+
+    // Si on ne connait pas les angles
+    if (tetaIsCalc == false) {
+        // On calcul teta2 grâce à la fermeture géométrique inverse
+        tmp = this->l * this->l;
+        tmp+= (this->h+VAL_D-VAL_A)*(this->h+VAL_D-VAL_A);
+        tmp-=VAL_B^2-VAL_C^2;
+        tmp/=(2*VAL_B*VAL_C);
+
+        if (tmp>1) tmp = 1;
+        else if (tmp<-1) tmp=-1;
+
+        this->teta2 = -acos(tmp);
+
+        // On fait une dichotomie pour trouver teta1
+        teta1Min = -90;
+        teta1Max = 90;
+        tmp = this->l - calcCoordOfL( (teta1Min+teta1Max)/2, this->teta2);
+        while ( abs(tmp) > DELTA_L) {
+            if (tmp > 0) {
+                teta1Max = (teta1Min+teta1Max)/2;
+            }else{
+                teta1Min = (teta1Min+teta1Max)/2;
+            }
+        }
+        
+        this->teta1 = (teta1Min+teta1Max)/2;
+    }
+
+    teta1 = this->teta1;
+    teta2 = this->teta2;
 }
 
 void GeoPlane::getCoord(float & l, float & h) {
+    // Si on ne connait pas les coordonées dans le repère
     if (coordIsCalc == false) {
         this->l = calcCoordOfL(teta1, teta2);
         this->h = calcCoordOfH(teta1, teta2);
