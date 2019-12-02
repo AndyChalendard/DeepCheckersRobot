135936411439e52642eae774c53231feded4fc08
Andy <chalendard.andy@gmail.com>
2019-12-03 00:32:24 +0100

    Correctif

--- Game/board.py
+++ Game/board.py
@@ -22,13 +22,13 @@ class Board:
     SIZE_X=12
     SIZE_Y=8
     def __init__(self): 
-        self._board=np.zeros((self.SIZE_X,self.SIZE_Y/2))
+        self._board=np.zeros((self.SIZE_X,self.SIZE_Y//2))
         self.reset()
     
     def reset(self):
         for i in range (self.SIZE_X):
-            for j in range (self.SIZE_Y/2):
-                if (i<(self.SIZE_X -2)/2):
+            for j in range (self.SIZE_Y//2):
+                if (i<(self.SIZE_X -2)//2):
                     self._board[i][j] = Pawns.BLUE
                 elif (i>=(self.SIZE_X -2)/2 + 2):
                     self._board[i][j] = Pawns.RED
@@ -36,14 +36,14 @@ class Board:
     def display(self):
         for i in range (self.SIZE_X):
             line = '|'
-            for j in range (self.SIZE_Y/2):
+            for j in range (self.SIZE_Y//2):
                 if j%2 == i%2:
                     line = line + Pawns.display(self._board[i][j]) + '|'
                 else:
                     line = line +'   '+ '|' + Pawns.display(self._board[i][j]) + '|' +'   '+ '|'
             print (line)
 
-if __name__ == "__main__": #tests    
+if __name__ == "__main__": #tests
     b=Board()
     b.display()
 
